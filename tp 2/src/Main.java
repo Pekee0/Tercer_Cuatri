6379749fdd61e474882751fc34e18f4ab684d77a
Pekee0 <gonza.marsala@gmail.com>
2024-03-20 11:02:17 -0300

    se agrega el tp 2

---
+++ tp 2/src/Main.java
@@ -0,0 +1,14 @@
+
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+       // Scanner teclado = new Scanner(System.in);
+
+        Rectangulo rec1 = new Rectangulo();
+
+        //System.out.println("Ingrese un valor para el alto del recatangulo. ");
+
+    }
+}
